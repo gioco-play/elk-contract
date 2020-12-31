64d75fccdd0d1e13dd2f41d9b9c20460e07710a7
Xuan Yang <xuan.yang@goldenf.co>
2020-12-31 14:41:27 +0800

    init

---
+++ src/Contract/ELKServiceInterface.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+namespace GiocoPlus\ELK\Contract;
+/**
+ * ELK
+ */
+interface ELKServiceInterface {
+
+    /**
+     * 提交資料至ELK
+     *
+     * @param string $uri
+     * @param array $data
+     * @return void
+     */
+    function post(string $uri, array $data): void;
+}
+
