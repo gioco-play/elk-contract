cb3f5f6edeb3b5d92e4a0128d3b3a0e018ad2974
tkd9714 <tkd9714@hotmail.com>
2021-03-03 11:59:01 +0800

    新增gfRequestVendor接口

--- src/Contract/ELKServiceInterface.php
+++ src/Contract/ELKServiceInterface.php
@@ -7,6 +7,34 @@ namespace GiocoPlus\ELK\Contract;
 interface ELKServiceInterface {
 
     /**
+     * GF 請求 vendor 的 log
+     * @param string $uri   // ELK 請求接口
+     * @param string $vendor_code   // 遊戲商代碼
+     * @param string $op_code       // 營運商代碼
+     * @param string $host          // 請求url
+     * @param string $url           // 請求url function
+     * @param array $request        // 請求參數
+     * @param float $request_time   // 請求執行時間
+     * @param int $http_code        // http code
+     * @param array $header         // response header
+     * @param string $response      // vendor response
+     * @param int $created_at       // 建立時間
+     * @return mixed
+     */
+    function gfRequestVendor(string $uri,
+                             string $vendor_code,
+                             string $op_code,
+                             string $host,
+                             string $url,
+                             array $request,
+                             float $request_time,
+                             int $http_code,
+                             array $header,
+                             string $response,
+                             int $created_at
+    );
+
+    /**
      * 提交資料至ELK
      *
      * @param string $uri
