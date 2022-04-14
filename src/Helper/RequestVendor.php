63de784197250208fbf9b621dd0ecce8a70f29d0
q90016200 <q90016200@gmail.com>
2022-04-14 15:54:15 +0800

    up

--- src/Helper/RequestVendor.php
+++ src/Helper/RequestVendor.php
@@ -105,7 +105,7 @@ class RequestVendor
         $this->requestTime = $requestTime;
 
         $this->response = $response;
-        $this->responseHttpCode = (method_exists($resp, 'getStatusCode')) ? $resp->getStatusCode() : '';
+        $this->responseHttpCode = (method_exists($resp, 'getStatusCode')) ? $resp->getStatusCode() : 0;
         $this->responseHeaders = (method_exists($resp, 'getHeaders')) ? $resp->getHeaders() : [];
     }
 }
