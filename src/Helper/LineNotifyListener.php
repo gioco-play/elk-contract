7aabbe076ad70388a0b4f8ec41d0ba9861613b45
q90016200 <q90016200@gmail.com>
2023-05-05 10:49:58 +0800

    fix ln timeout

--- src/Helper/LineNotifyListener.php
+++ src/Helper/LineNotifyListener.php
@@ -70,7 +70,7 @@ class LineNotifyListener implements ListenerInterface {
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
+            CURLOPT_TIMEOUT => 30,
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => 'POST',
